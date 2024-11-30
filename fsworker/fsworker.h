9714a64a332009ae62fa8ca7b8775b0b47bfe770
coolhezker <coolhezker@shalqi.com>
2024-12-01 00:27:22 +0300

    Пока что реализовал несколько простых процедур с fstream. ReaderVec и ReWriterVec было бы неплохо превратить в просто перегруженный ReWriter, который бы мог в качестве аргумента принимать любой контейнер(нереализуемо ввиду разности структут контейнеров, поэтому просто сделаю ReWriter для нескольких контейнеров из STL)

--- fsworker/fsworker.h
+++ fsworker/fsworker.h
@@ -2,38 +2,84 @@
 #define FSWORKER_H
 
 #include <string>
+#include <vector>
 #include <fstream>
 #include <filesystem>
 #include <exception>
 
-namespace fswconcepts {
+using namespace std;
+
+namespace fsw_c {
 	template<typename T>
-	concept OFStreamC = requires (T t, std::ofstream & ofs) {
-		{ ofs >> t } -> std::same_as<std::ofstream&>;
+	concept OStreamC = requires (T t, ostream & ofs) {
+		{ ofs << t } -> same_as<ostream&>;
 	};
 
 	template<typename T>
-	concept IFStreamC = requires (T t, std::ifstream & ifs) {
-		{ ifs >> t } -> std::same_as<std::ifstream&>;
+	concept IStreamC = requires (T t, istream & ifs) {
+		{ ifs >> t } -> same_as<istream&>;
 	};
 
 	template<typename T>
 	concept ToPathFS = requires(T t) {
-		{ static_cast<std::filesystem::path>(t) } -> std::same_as<std::filesystem::path>;
+		{ static_cast<filesystem::path>(t) } -> same_as<filesystem::path>;
+		{ static_cast<string>(t) } -> same_as<string>;
 	};
 }
 
-class FSWException : public std::exception {
-private:
-	std::string Message;
-public:
-	const char* what() const noexcept override {
-		return Message.c_str();
-	}
-};
-
-namespace fsworker {
-	
+namespace fsw_e {
+	class FSWException : public exception {
+	private:
+		string Message;
+	public:
+		FSWException(const string& msg) : Message{ msg } {};
+		const char* what() const noexcept override {
+			return Message.c_str();
+		}
+	};
 }
 
-#endif
+namespace fsw {
+	template<fsw_c::IStreamC Object, fsw_c::ToPathFS PathFS>
+	void Reader(Object& obj, PathFS pfs) {
+		ifstream ifs;
+		ifs.open(pfs, ios::in);
+		if (!ifs.is_open())
+			throw fsw_e::FSWException("Givenchi");
+		ifs >> obj;
+	}
+
+	template<fsw_c::IStreamC Object, fsw_c::ToPathFS PathFS>
+	void ReaderVec(vector<Object>& vobj, PathFS pfs) {
+		ifstream ifs;
+		Object buf;
+		ifs.open(pfs, ios::in);
+		if(!ifs.is_open()) 
+			throw fsw_e::FSWException("Houdini");
+		while (ifs >> buf) {
+			vobj.push_back(buf);
+		}
+	}
+
+	template<fsw_c::OStreamC Object, fsw_c::ToPathFS PathFS>
+	void ReWriter(Object& obj, PathFS pfs) {
+		ofstream ofs;
+		ofs.open(pfs, ios::out, ios::trunc);
+		if (!ofs.is_open())
+			throw fsw_e::FSWException("Gulfik");
+		ofs << obj;
+	}
+
+	template<fsw_c::OStreamC Object, fsw_c::ToPathFS PathFS>
+	void ReWriterVec(vector<Object>& vobj, PathFS pfs) {
+		ofstream ofs;
+		ofs.open(pfs, ios::out, ios::trunc);
+		if (!ofs.is_open())
+			throw fsw_e::FSWException("Gulfik");
+		for (auto it = vobj.begin(); it != vobj.end(); it++) {
+			ofs << *it;
+		}
+	}
+}
+
+#endif // !FSWORKER_H
